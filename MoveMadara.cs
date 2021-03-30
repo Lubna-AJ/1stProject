002a9573996e56a21924c8894fbfaae432106035
Lubna-AJ <81633208+Lubna-AJ@users.noreply.github.com>
2021-03-30 17:47:47 +0300

    Add files via upload

---
+++ MoveMadara.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMadara : MonoBehaviour {
+
+
+    float dirX;
+    [SerializeField]
+    float moveSpeed = 5f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        dirX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        transform.position = new Vector2(transform.position.x + dirX, transform.position.y);
+    }
+}
