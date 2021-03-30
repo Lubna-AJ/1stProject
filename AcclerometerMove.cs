d83889610b8651edecd9b7da67939b1ce9b8fe9a
Lubna-AJ <81633208+Lubna-AJ@users.noreply.github.com>
2021-03-30 17:52:10 +0300

    Add files via upload

---
+++ AcclerometerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AcclerometerMove : MonoBehaviour {
+
+    public float speed = 20f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float temp =Input.acceleration.x;
+        float z = Input.acceleration.z;
+        Debug.Log(temp);
+        transform.Translate(0, 0, -z * speed * Time.deltaTime);
+        transform.Translate(0, 0, -temp * speed);
+        
+        
+		
+	}
+}
